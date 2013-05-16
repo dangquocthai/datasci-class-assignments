42f49bc3d7f416461a4fd48eb78a126cae31f581
Rishabh Sagar <rishabh.sagar@gmail.com>
2013-05-16 16:20:21 +0100

    Initial solution to p2

--- assignment1/tweet_sentiment.py
+++ assignment1/tweet_sentiment.py
@@ -1,17 +1,46 @@
 import sys
+import json
 
-def hw():
-    print 'Hello, world!'
+sentiments = {}
+tweets = []
 
-def lines(fp):
-    print str(len(fp.readlines()))
+def print_stats():
+  print "Number of sentiments : " + str(len(sentiments.keys()))
+  print "Sample of sentiments"
+  sample_keys = sentiments.keys()[0:5]
+  for key in sample_keys : print key + " => " + str(sentiments[key])
+  print "-------------------------------"
+  print "Number of tweets : " + str(len(tweets))
+  print "Sample of tweets"
+  for index in range(0,5):
+    print tweets[index]
+
+def build_sentiment_dict(sent_file):
+  for line in sent_file.readlines():
+    sentiment, score = line.split("\t")
+    sentiments[sentiment] = int(score)
+
+def build_tweet_list(tweet_file):
+  for line in tweet_file.readlines():
+    tweet = json.loads(line)
+    if "text" in tweet.keys():
+      tweets.append(tweet["text"])
 
 def main():
-    sent_file = open(sys.argv[1])
-    tweet_file = open(sys.argv[2])
-    hw()
-    lines(sent_file)
-    lines(tweet_file)
+  # Get arguments
+  sent_file = open(sys.argv[1])
+  tweet_file = open(sys.argv[2])
+  # Load files to data structs.
+  build_sentiment_dict(sent_file)
+  build_tweet_list(tweet_file)
+
+  # For debugging.
+  #print_stats()
+
+  # Scoring.
+  for tweet in tweets:
+    print tweet.encode('ascii', 'ignore')
+    print float(sum(map(lambda word: sentiments.get(word, 0), tweet.split(' '))))
 
 if __name__ == '__main__':
-    main()
+  main()
