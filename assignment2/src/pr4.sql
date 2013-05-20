fde8eba575550eff19769e1877165c26b230c556
Rishabh Sagar <Rishabh.Sagar@gmail.com>
2013-05-20 01:20:13 +0100

    correct issue with group by clause

--- assignment2/src/pr4.sql
+++ assignment2/src/pr4.sql
@@ -8,7 +8,7 @@ select f1.docid, sum(f1.count * f2.count) as count from frequency f1,
     SELECT 'q' as docid, 'treasury' as term, 1 as count
 ) as f2
 where f1.term = f2.term
-group by f1.docid = f2.docid
+group by f1.docid, f2.docid
 order by count desc
 limit 1;
 
